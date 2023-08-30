5cce7832edd347d62b23028d01284a9e06972eba
Rob Young <rhyoung214@gmail.com>
2023-08-29 20:36:24 -0700

    add rubocop and settings, added static_pages controller and index view

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
+  root "static_pages#index"
+  get "static_pages/index", to: "static_page#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
